6303ed2572ce625e2c6fd0fbf4b5f57d2fd679bb
damjanh <damjan.hudobreznik@gmail.com>
2018-02-12 21:02:09 +0100

    Basic MusicManager
    Implement a music manager for playing audio clips for different levels.

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicManager : MonoBehaviour {
+
+	private AudioSource audioSource;
+
+	public AudioClip[] levelMusicArray;
+
+	void Awake() {
+		// Persist this game object
+		DontDestroyOnLoad(gameObject);
+	}
+
+	// Use this for initialization
+	void Start () {
+		audioSource = GetComponent<AudioSource>();
+	}
+
+	void OnLevelWasLoaded(int level) {
+		AudioClip thisLevelMusic = levelMusicArray[level];
+		// If we have an AudioClip associated with a level index play it.
+		if (thisLevelMusic != null) {
+			audioSource.clip = thisLevelMusic;
+			audioSource.loop = true;
+			audioSource.Play();
+		}
+	}
+}
