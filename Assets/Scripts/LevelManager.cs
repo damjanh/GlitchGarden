6797f554b076057ce9faca8090428b6b0a96a632
damjanh <damjan.hudobreznik@gmail.com>
2018-02-12 19:14:52 +0100

    Initial commit.
    First commit. Implemented basic splash screen and a menu.

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	public float autoLoadNextLevel = 3f;
+
+	void Start()
+	{
+		Invoke("LoadNextLevel", autoLoadNextLevel);
+	}
+
+	public void LoadLevel (string levelName)
+	{
+		SceneManager.LoadScene(levelName, LoadSceneMode.Single);
+	}
+
+	public void Quit ()
+	{
+		Application.Quit();
+	}
+
+	public void LoadNextLevel()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+An effort to implement a clone of the "Plant vs Zombies" game.
+
+Following the online tutorial course for Unity by Ben Tristem:
+https://www.udemy.com/unitycourse/learn/v4/overview
+
+Using assets from the failed game that are now in the public domain:
+https://www.glitchthegame.com/public-domain-game-art/
+
+Fonts from:
+https://www.dafont.com/
+
+Music from:
+https://freesound.org/
+
+
