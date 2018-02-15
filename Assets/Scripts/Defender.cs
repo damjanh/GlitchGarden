2937c68e170743732dbbb60e6295fac0f19032ec
damjanh <damjan.hudobreznik@gmail.com>
2018-02-15 20:44:30 +0100

    Add coliders and Defender script.
     - Add coliders to prefabs to fire triggers.
     - Ad Defender script.
     - Save all to prefabs.

--- Assets/Scripts/Defender.cs
+++ Assets/Scripts/Defender.cs
@@ -2,20 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Attacker : MonoBehaviour {
-
-	[Range (0f, 1.5f)]
-	public float walkSpeed;
+public class Defender : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		Rigidbody2D rigidbody = gameObject.AddComponent<Rigidbody2D>();
-		rigidbody.isKinematic = true;
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Translate(Vector3.left * walkSpeed * Time.deltaTime);
+		
 	}
 
 	void OnTriggerEnter2D() {
