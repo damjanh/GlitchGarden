e2d73467cded0270edee4a0f28fca3ed4f78e9da
damjanh <damjan.hudobreznik@gmail.com>
2018-02-21 19:37:23 +0100

    Shooter and Projectile Implementation
     - Implement the Shooter and Projectile scripts.
     - Add Gun game object as origin for spawning the Projectiles.
     - Add Shredder.
     - Prefab changes on Defenders.
     - Add new Prefabs.

--- Assets/Scripts/Defender.cs
+++ Assets/Scripts/Defender.cs
@@ -3,18 +3,5 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Defender : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-	void OnTriggerEnter2D() {
-		Debug.Log(name + " trigger enter.");
-	}
+	// Used as a tag.
 }
---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour {
+
+	public GameObject projectile;
+
+	public GameObject parent;
+
+	public GameObject gun;
+
+	private void Fire() {
+		GameObject newProjectile = Instantiate(projectile) as GameObject;
+		newProjectile.transform.parent = parent.transform;
+		newProjectile.transform.position = gun.transform.position;
+	}
+}
