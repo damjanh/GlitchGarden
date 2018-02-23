40f4b81864bdcf6b5c0a75c5cea928dcb5c84f0a
damjanh <damjan.hudobreznik@gmail.com>
2018-02-23 18:56:35 +0100

    Shooter optimization
     - Find or create Projectile game object when instantiating new projectiles.
     - Remove Projectiles parent prefab.

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class Shooter : MonoBehaviour {
 
-	public GameObject projectile;
-
-	public GameObject parent;
+	public  GameObject projectile;
 
 	public GameObject gun;
+
+	private GameObject parent;
+
+	void Start() {
+		parent = GameObject.Find("Projectiles");
+		if (parent == null) {
+			parent = new GameObject("Projectiles");
+		}
+	}
 
 	private void Fire() {
 		GameObject newProjectile = Instantiate(projectile) as GameObject;
