5ae6e2ec6f5ca0d980624872341475b027ee28d0
damjanh <damjan.hudobreznik@gmail.com>
2018-02-24 16:54:39 +0100

    Game timer & win condition.
     - Implement a time based win condition.
     - Show win text  and play win sound  clip when level time is reached.
     - Load next level when win sound clip finishes.
     - Remove test win button.

---
+++ Assets/Scripts/GameTimer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour {
+
+	private Slider slider;
+
+	public float levelTimeSeconds = 60;
+
+	private AudioSource audioSource;
+
+	private LevelManager levelManager;
+
+	private GameObject winText;
+
+	private bool isEnd = false;
+
+	// Use this for initialization
+	void Start () {
+		slider = GetComponent<Slider>();
+		audioSource = GetComponent<AudioSource>();
+		levelManager = GameObject.FindObjectOfType<LevelManager>();
+
+		winText = GameObject.Find("WinText");
+		winText.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		slider.value = Time.timeSinceLevelLoad / levelTimeSeconds;
+		if (!isEnd && Time.timeSinceLevelLoad >= levelTimeSeconds) {
+			isEnd = true;
+			audioSource.volume = PlayerPrefsManager.GetMasterVolume();
+			audioSource.Play();
+			winText.SetActive(true);
+			Invoke("LoadNextLevel", audioSource.clip.length);
+		}
+	}
+
+	private void LoadNextLevel() {
+		levelManager.LoadNextLevel();
+	}
+}
