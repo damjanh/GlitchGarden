89d98beec637e6f740b1aacee4d1142ba5ed4838
damjanh <damjan.hudobreznik@gmail.com>
2018-02-24 19:31:19 +0100

    Gravestone under attack animation & sorting order fix.
      - Implement gravestone under attack animation.
      - Change sorting order of attackers and projectiles.

--- Assets/Scripts/Gravestone.cs
+++ Assets/Scripts/Gravestone.cs
@@ -3,5 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Gravestone : MonoBehaviour {
-	// Used as a tag.
+
+	private Animator animator;
+
+	void Start() {
+		animator = GetComponent<Animator>();
+	}
+	
+	void OnTriggerStay2D(Collider2D collider) {
+		GameObject obj = collider.gameObject;
+		// Animated under attack when triggering with any Attacker, but not with the Fox.
+		if (obj.GetComponent<Attacker>() && obj.GetComponent<Fox>() == null) {
+			animator.SetTrigger("underAttackTrigger");
+		}
+	}
 }
